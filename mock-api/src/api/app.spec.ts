c5174d24ceafb8598b2f7ad9d25b449accc380bc
hevans90 <hevans9000@gmail.com>
2018-04-09 11:57:25 +0100

    test: add tests for 404 & ./plans

--- mock-api/src/api/app.spec.ts
+++ mock-api/src/api/app.spec.ts
@@ -1,9 +1,34 @@
-describe('A suite is just a function', function() {
-  var a;
+import * as request from 'supertest';
+import { Server } from 'http';
+import { finishTest } from '../../spec/tools/finish-test';
 
-  it('and so is a spec', function() {
-    a = false;
+import { App } from './app';
+import { serverRunner } from './server-runner';
 
-    expect(a).toBe(false);
+describe('App', function() {
+  let server: Server;
+  const port = 3000;
+
+  beforeEach(done => {
+    console.log(`\nRestarting server...`);
+    server = serverRunner(port, false, done);
+  });
+
+  afterEach(done => {
+    server.close(done);
+  });
+
+  it('should respond 404 to unknown routes', done => {
+    request(server)
+      .get('/nonsense_route')
+      .expect(404, finishTest(done));
+  });
+
+  describe('/plans', () => {
+    it('should respond 200', done => {
+      request(server)
+        .get('/plans')
+        .expect(200, finishTest(done));
+    });
   });
 });
