860070996d20e89e40648b5c7d6d76c3dfac2e0c
Harri <hevans9000@gmail.com>
2018-04-10 17:49:53 +0100

    feat: working POC for API testing CLI

--- api-testing/app/program.ts
+++ api-testing/app/program.ts
@@ -1,16 +1,39 @@
 import * as colors from 'colors';
 import * as request from 'request';
+import * as fs from 'fs';
+import * as jsYaml from 'js-yaml';
+
+import {
+  OpenApiValidator,
+  ValidationError
+} from 'express-openapi-validate/dist';
 
 const apiUrl = 'http://localhost:4040';
 
+const openApiDocument = jsYaml.safeLoad(
+  fs.readFileSync('../swagger.yaml', 'utf-8')
+);
+
+const validator = new OpenApiValidator(openApiDocument);
+
 const callPlans = () => {
-  request.get(
+  const validateResponse = validator.validateResponse('get', '/plans');
+  let res = request.get(
     `${apiUrl}/plans`,
-    (err: any, req: request.Response, body: any) => {
-      console.log(JSON.stringify(body));
+    (err: any, res: request.Response, body: string) => {
+      res.body = JSON.parse(res.body);
+
+      try {
+        validateResponse(res);
+      } catch (err) {
+        let xerr: ValidationError = err;
+        console.log(xerr.message);
+      }
     }
   );
+  console.log(validateResponse);
 };
 
 console.log(`\n${colors.bgBlue(colors.bold('--API Testing CLI--'))}\n`);
+
 callPlans();
