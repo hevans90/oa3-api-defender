e4daa029e29523f00b1f7e6ff3b8d285ec6069c6
hevans90 <hevans9000@gmail.com>
2018-04-12 12:18:20 +0100

    refactor(strictTS): add strict flags & minor refactors to compensate

--- mock-api/app/middleware/request-reporter.ts
+++ mock-api/app/middleware/request-reporter.ts
@@ -7,15 +7,15 @@ export const reqReporter = (req: Request, res: Response, next): void => {
 };
 
 const statusString = (req: Request, res: Response): string => {
-  function httpVerbWithColor(req: Request, res: Response): string {
-    if (res.statusCode.toString().startsWith('4')) {
-      return colors.red.bold(req.method);
-    } else {
-      return colors.green.bold(req.method);
-    }
-  }
-
   return `${httpVerbWithColor(req, res)} ${req.path} -- ${res.statusCode} : ${
     res.statusMessage
   }`;
+};
+
+const httpVerbWithColor = (req: Request, res: Response): string => {
+  if (res.statusCode.toString().startsWith('4')) {
+    return colors.red.bold(req.method);
+  } else {
+    return colors.green.bold(req.method);
+  }
 };
