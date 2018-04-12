a0096f34f6a84524a7163d3f211a171b19159015
hevans90 <hevans9000@gmail.com>
2018-04-12 15:38:27 +0100

    refactor(api-testing): endpoint-validator re-usable class

---
+++ api-testing/app/endpoint-validator.ts
@@ -0,0 +1,35 @@
+import {
+  OpenApiValidator,
+  ValidationError
+} from 'express-openapi-validate/dist';
+import { Operation } from 'express-openapi-validate/dist/OpenApiDocument';
+
+import * as request from 'request';
+import { ErrorFormatter } from './error-formatter';
+
+export abstract class EndpointValidator {
+  public static validate(
+    validator: OpenApiValidator,
+    operation: Operation,
+    path: string,
+    rootUrl: string
+  ) {
+    const validateResponse = validator.validateResponse(operation, `/${path}`);
+    const response = request.get(
+      `${rootUrl}/${path}`,
+      (err: any, res: request.Response, body: string) => {
+        res.body = JSON.parse(res.body);
+
+        try {
+          validateResponse(res);
+        } catch (error) {
+          const xerr: ValidationError = error;
+
+          xerr.data.forEach(errorObj => {
+            console.log(ErrorFormatter.formatError(errorObj));
+          });
+        }
+      }
+    );
+  }
+}
--- api-testing/app/program.ts
+++ api-testing/app/program.ts
@@ -1,14 +1,12 @@
 import * as colors from 'colors';
-import * as request from 'request';
 import * as fs from 'fs';
 import * as jsYaml from 'js-yaml';
-
-import { ErrorFormatter } from './error-formatter';
 
 import {
   OpenApiValidator,
   ValidationError
 } from 'express-openapi-validate/dist';
+import { EndpointValidator } from './endpoint-validator';
 
 const apiUrl = 'http://localhost:4040';
 
@@ -17,29 +15,9 @@ const openApiDocument = jsYaml.safeLoad(
 );
 
 const validator = new OpenApiValidator(openApiDocument, {
-  ajvOptions: { allErrors: true, verbose: false }
+  ajvOptions: { allErrors: true, verbose: true }
 });
-
-const callPlans = () => {
-  const validateResponse = validator.validateResponse('get', '/plans');
-  const response = request.get(
-    `${apiUrl}/plans`,
-    (err: any, res: request.Response, body: string) => {
-      res.body = JSON.parse(res.body);
-
-      try {
-        validateResponse(res);
-      } catch (error) {
-        const xerr: ValidationError = error;
-
-        xerr.data.forEach(errorObj => {
-          console.log(ErrorFormatter.formatError(errorObj));
-        });
-      }
-    }
-  );
-};
 
 console.log(`\n${colors.bgBlue(colors.bold('--API Testing CLI--'))}\n`);
 
-callPlans();
+EndpointValidator.validate(validator, 'get', 'plans', apiUrl);
