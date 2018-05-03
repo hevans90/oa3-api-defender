9235ef0b614fa039129fd9632a5ddf960a3ce645
hevans90 <hevans9000@gmail.com>
2018-05-03 10:41:15 +0100

    feat: recursive request-body parsing

--- src/request-parser.ts
+++ src/request-parser.ts
@@ -7,6 +7,7 @@ import {
   ReferenceObject,
 } from 'express-openapi-validate/dist/OpenApiDocument';
 import { Schema } from 'js-yaml';
+import { isObject } from 'util';
 
 const debug = Debug('oa3-def');
 
@@ -29,15 +30,17 @@ export class RequestParser {
 
       let requestSchema: SchemaObject | ReferenceObject | undefined =
         opConfig.config.requestBody.content['application/json'].schema;
-      // debug(`initial Schema: ${JSON.stringify(requestSchema, null, 2)}`);
 
       if (requestSchema) {
         requestSchema = resolveReference(document, requestSchema);
-        // debug(`resolved Schema: ${JSON.stringify(requestSchema, null, 2)}`);
 
         if (requestSchema.properties) {
           Object.keys(requestSchema.properties).forEach(prop => {
-            const ex = (requestSchema as any).properties[prop].example;
+            const ref = resolveReference(
+              document,
+              (requestSchema as any).properties[prop],
+            );
+            const ex = this.generateExampleValue(ref);
             reqBody[prop] = ex ? ex : undefined;
           });
           debug(`Generated Body: ${JSON.stringify(reqBody, null, 2)}`);
@@ -46,5 +49,25 @@ export class RequestParser {
     }
 
     return reqBody;
+  }
+
+  /**
+   * Recursive function to generate examples for individual SchemaObjects.
+   * If an object literal is detected the function will recurse down the tree and look for examples, indefinitely.
+   */
+  public static generateExampleValue(schema: SchemaObject | any): any {
+    if (schema.example) {
+      return schema.example;
+    } else if (schema.properties) {
+      const exampleLiteral = {};
+
+      Object.keys(schema.properties).forEach(prop => {
+        exampleLiteral[prop] = this.generateExampleValue(
+          schema.properties[prop],
+        );
+      });
+
+      return exampleLiteral;
+    }
   }
 }
